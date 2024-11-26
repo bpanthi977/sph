cf73a881b3a2a082fb32524d298431152d09357c
Bibek Panthi <bpanthi977@gmail.com>
2024-11-26 13:01:23 -0600

    🎁 Improve renderer

--- visualize.lisp
+++ visualize.lisp
@@ -2,20 +2,28 @@
 (require :float-features)
 (require :fast-io)
 
+(defpackage visualize
+  (:use :cl))
+
+(in-package visualize)
+
 (defconstant +h+ 1/24 "Kernel Support Radius")
 (defconstant +d+ (/ +h+ 1.2d0) "Particle spacing")
 
 ;;; Particle
 
 (defstruct particle
-  (x 0d0 :type double-float)
-  (y 0d0 :type double-float)
-  (pressure 0d0 :type double-float))
+  (x 0.0 :type short-float)
+  (y 0.0 :type short-float)
+  (vx 0.0 :type short-float)
+  (vy 0.0 :type short-float)
+  (mass 0.0 :type short-float)
+  (pressure 0.0 :type short-float))
 
 (defmethod print-object ((o particle) stream)
-  (with-slots (x y pressure) o
-    (format stream "#S(PARTICLE (~,2e, ~,2e) :pressure ~,2e)"
-            x y pressure)))
+  (with-slots (x y) o
+    (format stream "#S(PARTICLE (~,2e, ~,2e))"
+            x y)))
 
 (defun draw-circle (renderer cx cy r)
   "Midpoint Circle Algorithm"
@@ -23,14 +31,14 @@
          (y 0)
          (err (- 3 (* 2 r))))
     (flet ((draw-point (x y)
-             (sdl2:render-draw-point renderer (+ cx x) (- cy y))
-             (sdl2:render-draw-point renderer (+ cx x) (+ cy y))
-             (sdl2:render-draw-point renderer (- cx x) (- cy y))
-             (sdl2:render-draw-point renderer (- cx x) (+ cy y))
-             (sdl2:render-draw-point renderer (+ cx y) (- cy x))
-             (sdl2:render-draw-point renderer (+ cx y) (+ cy x))
-             (sdl2:render-draw-point renderer (- cx y) (- cy x))
-             (sdl2:render-draw-point renderer (- cx y) (+ cy x))))
+             (funcall renderer (+ cx x) (- cy y))
+             (funcall renderer (+ cx x) (+ cy y))
+             (funcall renderer (- cx x) (- cy y))
+             (funcall renderer (- cx x) (+ cy y))
+             (funcall renderer (+ cx y) (- cy x))
+             (funcall renderer (+ cx y) (+ cy x))
+             (funcall renderer (- cx y) (- cy x))
+             (funcall renderer (- cx y) (+ cy x))))
 
       (loop while (>= x y) do
         (draw-point x y)
@@ -41,32 +49,41 @@
               (t
                (incf err (* 2 (+ 3 (* 2 y))))))))))
 
-(defun draw (renderer particles)
+(defparameter *translate-x* 200)
+(defparameter *translate-y* 200)
+(defparameter *scale* 200)
+
+(defun draw (draw-point set-color particles)
   (flet ((transform-x (x)
            ;; x is in m
            ;; take 500 pixels = 1m
-           (truncate (+ 200 (* 250 x))))
+           (truncate (+ *translate-x* (* *scale* x))))
          (transform-y (y)
-           (truncate (- 200 (* 250 y)))))
+           (truncate (- *translate-y* (* *scale* y)))))
 
     (loop for p across particles
           with max-pressure = (max 1 (reduce #'max particles :key #'particle-pressure :initial-value 0))
-          with r = (truncate (* 250 1/2 +d+))
+          with r = (truncate (* *scale* 1/2 +d+))
           for x = (particle-x p)
           for y = (particle-y p) do
-            (sdl2:set-render-draw-color renderer
-                                        (floor (* 255 (min 1 (/ (particle-pressure p)
-                                                                max-pressure))))
-                                        0 0
-                                        sdl2-ffi:+sdl-alpha-opaque+)
+            (funcall set-color
+                     (floor (* 255 (min 1 (/ (particle-pressure p)
+                                             max-pressure))))
+                     0 255)
+            (draw-circle draw-point (transform-x x) (transform-y y) r))))
 
-            (draw-circle renderer (transform-x x) (transform-y y) r))))
+(defconstant +SIM-LITTLE-ENDIAN+     #b00001)
+(defconstant +SIM-MASS+              #b00010)
+(defconstant +SIM-PRESSURE+          #b00100)
+(defconstant +SIM-VELOCITY+          #b01000)
 
 (defclass simulation ()
   ((io-buffer :initarg :io-buffer)
    (header-end-position)
+   (flags :initarg :flags :accessor flags)
    (little-endian-p :initarg :little-endian-p)
    (ended :initform nil :accessor ended)
+   (world-time :initform 0 :accessor world-time)
    (particles :accessor particles)
    (frame-number :accessor frame-number :initform 0)))
 
@@ -78,26 +95,27 @@
 
 (defmethod read-u32 ((s simulation))
   (with-slots (little-endian-p io-buffer) s
-      (if little-endian-p
-          (fast-io:read32-le io-buffer)
-          (fast-io:read32-be io-buffer))))
+    (if little-endian-p
+        (fast-io:read32-le io-buffer)
+        (fast-io:read32-be io-buffer))))
 
-(defmethod read-double ((s simulation))
+(defmethod read-single ((s simulation))
   (declare (optimize (debug 3)))
   (with-slots (little-endian-p io-buffer) s
-      (float-features:bits-double-float
-                (if little-endian-p
-                    (fast-io:readu64-le io-buffer)
-                    (fast-io:readu64-be io-buffer)))))
+    (float-features:bits-single-float
+     (if little-endian-p
+         (fast-io:readu32-le io-buffer)
+         (fast-io:readu32-be io-buffer)))))
 
 (defun open-simulation-file (filename)
   (let* ((stream (open filename :direction :input
                                 :element-type '(unsigned-byte 8)))
          (io-buffer (fast-io:make-input-buffer :stream stream))
-         (little-endian-p (case (fast-io:readu8 io-buffer)
+         (flags (fast-io:readu8 io-buffer))
+         (little-endian-p (case (logand +sim-little-endian+ flags)
                             (1 T)
                             (0 nil)))
-         (sim (make-instance 'simulation :io-buffer io-buffer :little-endian-p little-endian-p)))
+         (sim (make-instance 'simulation :io-buffer io-buffer :little-endian-p little-endian-p :flags flags)))
 
     (let* ((count (read-u32 sim))
            (particles (make-array count
@@ -105,9 +123,12 @@
                                   :initial-element nil)))
       (setf (slot-value sim 'header-end-position) (file-position stream))
       (loop for i from 0 below count do
-        (setf (aref particles i) (make-particle :x 0.0d0
-                                                :y 0.0d0
-                                                :pressure 0.0d0)))
+        (setf (aref particles i) (make-particle :x 0.0
+                                                :y 0.0
+                                                :mass (if (= 0 (logand +sim-mass+ flags))
+                                                          0.0
+                                                          (read-single sim)))))
+      (print flags)
       (setf (particles sim) particles)
       sim)))
 
@@ -124,10 +145,13 @@
       (let ((has-next-frame (read-u8 s)))
         (if has-next-frame
             (progn
+              (setf (world-time s) (read-single s))
               (loop for p across (particles s) do
-                (setf (particle-x p) (read-double s)
-                      (particle-y p) (read-double s)
-                      (particle-pressure p) (read-double s)))
+                (setf (particle-x p) (read-single s)
+                      (particle-y p) (read-single s)
+                      (particle-pressure p) (if (= 0 (logand +sim-pressure+ (flags s)))
+                                                0.0
+                                                (read-single s))))
               (incf (frame-number s)))
             (setf (ended s) t))
         (not (ended s)))))
@@ -141,39 +165,117 @@
      (unwind-protect (progn ,@body)
        (close-simulation ,simulation))))
 
+(defun gl-render (win gl sim)
+  (gl:clear :color-buffer)
+  (multiple-value-bind (width height) (sdl2:get-window-size win)
+    (let ((buffer (make-array (list height width 3) :element-type '(unsigned-byte 8)))
+          (r 0) (g 0) (b 0))
+      (flet ((draw-point (x y r g b)
+               (setf (aref buffer y x 0) r
+                     (aref buffer y x 1) g
+                     (aref buffer y x 2) b)))
+        (draw (lambda (x y)
+                (let ((x (floor x))
+                      (y (- height (floor y))))
+                  (when (and (<= 0 x (1- width))
+                             (<= 0 y (1- height)))
+                    (draw-point x y r g b))))
+              (lambda (_r _g _b)
+                (setf r _r g _g b _b))
+              (particles sim))
+
+        (gl:draw-pixels width height
+                        :rgb
+                        :unsigned-byte
+                        (make-array (* height width 3) :element-type '(unsigned-byte 8)
+                                                       :displaced-to buffer)))))
+  (gl:flush)
+  (sdl2:gl-swap-window win))
+
+(defun sdl-render (win renderer sim)
+  ;; Clear screen
+  (sdl2:set-render-draw-color renderer 255 255 255 255)
+  (sdl2:render-clear renderer)
+  ;; Draw objects
+  (draw (lambda (x y)
+          (sdl2:render-draw-point renderer x y))
+        (lambda (r g b)
+          (sdl2:set-render-draw-color renderer r g b sdl2-ffi:+sdl-alpha-opaque+))
+        (particles sim))
+  ;; Update screnn
+  (sdl2:render-present renderer))
+
+(defun render (win sim renderer gl)
+  (if nil
+      (sdl-render win renderer sim)
+      (gl-render win gl sim)))
 
 (defun main ()
   (sdl2:with-init (:everything)
     ;; Initialize Graphics
     (sdl2:with-window (win :w 1000 :h 600
                            :title "Physics simulation"
-                           :flags (list sdl2-ffi:+sdl-window-resizable+))
-      (sdl2:with-renderer (renderer win)
+                           :flags (list sdl2-ffi:+sdl-window-resizable+ sdl2-ffi:+sdl-window-opengl+))
+      (sdl2:with-gl-context (gl win)
+        ;;(sdl2:with-renderer (renderer win)
+        (sdl2:gl-make-current win gl)
         (with-simulation-file ("./out/results.data" sim)
-          (read-frame sim)
-          (sdl2:with-event-loop ()
-            (:quit () t)
-            (:window
-             (:type type)
-             (cond ((= sdl2-ffi:+sdl-windowevent-close+ type)
-                    (sdl2:push-event :quit))))
-            (:keydown
-             (:keysym keysym)
-             (let ((scancode (sdl2:scancode-value keysym)))
-               (case scancode
-                 (#.(list sdl2-ffi:+sdl-scancode-escape+ sdl2-ffi:+sdl-scancode-q+)
-                  (sdl2:push-event :quit))
-                 (#.sdl2-ffi:+sdl-scancode-space+
-                  (read-frame sim))
-                 (#.sdl2-ffi:+sdl-scancode-r+
-                  (reset-simulation sim)
-                  (read-frame sim)))))
-            (:idle
-             ()
-             ;; Clear screen
-             (sdl2:set-render-draw-color renderer 255 255 255 255)
-             (sdl2:render-clear renderer)
-             ;; Draw objects
-             (draw renderer (particles sim))
-             ;; Update screnn
-             (sdl2:render-present renderer))))))))
+          (let ((render-time-start (get-internal-real-time))
+                (last-frame nil)
+                (auto-play nil))
+            (read-frame sim)
+            (sdl2:with-event-loop ()
+              (:quit () t)
+              (:window
+               (:type type)
+               (cond ((= sdl2-ffi:+sdl-windowevent-close+ type)
+                      (sdl2:push-event :quit))))
+              (:keydown
+               (:keysym keysym)
+               (let ((scancode (sdl2:scancode-value keysym)))
+                 (case scancode
+                   (#.(list sdl2-ffi:+sdl-scancode-escape+ sdl2-ffi:+sdl-scancode-q+)
+                    (sdl2:push-event :quit))
+                   (#.sdl2-ffi:+sdl-scancode-f+
+                    (read-frame sim))
+                   (#.sdl2-ffi:+sdl-scancode-space+
+                    (if auto-play
+                        (setf auto-play nil)
+                        (progn
+                          (setf auto-play t
+                                render-time-start (- (get-internal-real-time)
+                                                     (* internal-time-units-per-second (world-time sim)))))))
+                   (#.sdl2-ffi:+sdl-scancode-r+
+                    (close-simulation sim)
+                    (setf sim (open-simulation-file "./out/results.data"))
+                    (setf render-time-start (get-internal-real-time))
+                    (read-frame sim))
+                   (#.sdl2-ffi:+sdl-scancode-up+
+                    (decf *translate-y* 25))
+                   (#.sdl2-ffi:+sdl-scancode-down+
+                    (decf *translate-y* -25))
+                   (#.sdl2-ffi:+sdl-scancode-left+
+                    (decf *translate-x* -25))
+                   (#.sdl2-ffi:+sdl-scancode-right+
+                    (decf *translate-x* 25))
+                   (#.sdl2-ffi:+sdl-scancode-minus+
+                    (setf *scale* (* *scale* 0.9)))
+                   (#.sdl2-ffi:+sdl-scancode-equals+
+                    (setf *scale* (/ *scale* 0.9))))))
+              (:idle
+               ()
+               ;; Read frames
+               (loop while
+                     (and auto-play
+                          (< (world-time sim)
+                             (/ (- (get-internal-real-time) render-time-start)
+                                internal-time-units-per-second))
+                          (not (ended sim)))
+                     do (read-frame sim))
+               (unless (eql (world-time sim) last-frame)
+                 (format t "T=~a (~f)~%" (world-time sim) (/ (- (get-internal-real-time) render-time-start)
+                                                             internal-time-units-per-second))
+                 (render win sim nil gl)
+                 (setf last-frame (world-time sim)))
+               (if (ended sim)
+                   (sleep 0.1))))))))))
--- world.cpp
+++ world.cpp
@@ -1,5 +1,7 @@
 #include "types.h"
 #include "kernel.h"
+#include <cassert>
+#include <cmath>
 #include <cstdint>
 #include <fstream>
 
@@ -22,9 +24,20 @@ vec2 World::external_acceleration(Particle &p) {
   return acc;
 }
 
+uint8_t SIM_LITTLE_ENDIAN = 0b00001;
+uint8_t SIM_MASS          = 0b00010;
+uint8_t SIM_PRESSURE      = 0b00100;
+uint8_t SIM_VELOCITY      = 0b01000;
+
+void write_single(std::ofstream &file, float s) {
+  file.write(reinterpret_cast<char *>(&s), sizeof(float));
+}
+
 void World::write_headers(std::ofstream &file) {
   // Write endianness
-  uint8_t flags = (std::endian::native == std::endian::little) ? 1 : 0;
+  uint8_t little_endian = (std::endian::native == std::endian::little) ? SIM_LITTLE_ENDIAN : 0;
+  uint8_t flags = little_endian | SIM_MASS;
+
   printf("Little Endian = %d\n", flags);
   file.write(reinterpret_cast<const char *>(&flags), sizeof(uint8_t));
 
@@ -32,19 +45,38 @@ void World::write_headers(std::ofstream &file) {
   uint32_t count = particles.size();
   printf("Count: %d\n", count);
   file.write(reinterpret_cast<const char*>(&count), sizeof(uint32_t));
+
+  // Mass of particles
+  for (Particle& p: particles) {
+    write_single(file, p.mass);
+  }
 }
 
 void World::write_frame(std::ofstream &file) {
   uint8_t next_frame = 1;
   file.write(reinterpret_cast<char *>(&next_frame), sizeof(uint8_t));
+  write_single(file, time);
   for (auto &p : particles) {
-    file.write(reinterpret_cast<char *>(&p.pos.x), sizeof(double));
-    file.write(reinterpret_cast<char *>(&p.pos.y), sizeof(double));
-    file.write(reinterpret_cast<char *>(&p.pressure), sizeof(double));
+    write_single(file, p.pos.x);
+    write_single(file, p.pos.y);
+    //write_single(file, p.pressure);
   }
 }
 
 void World::write_footers(std::ofstream &file) {
   uint8_t next_frame = 0;
   file.write(reinterpret_cast<char *>(&next_frame), sizeof(uint8_t));
+}
+
+void World::sanity_checks() {
+  // Check for NaNs
+  for (auto p: particles) {
+    assert(!isnan(p.pos.x));
+    assert(!isnan(p.pos.y));
+    assert(!isnan(p.vel.x));
+    assert(!isnan(p.vel.y));
+    assert(!isnan(p.vel_pred.x));
+    assert(!isnan(p.vel_pred.y));
+    assert(!isnan(p.alpha));
+  }
 }

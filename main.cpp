a5df5b64a795a33461ce5661307acfed1d2e0ae5
Bibek Panthi <bpanthi977@gmail.com>
2024-12-10 16:13:08 -0600

    Don't print timing information for every physics step

--- main.cpp
+++ main.cpp
@@ -198,8 +198,10 @@ int main(int argc, char** argv) {
       world->timer_end("Render");
     }
 
-    std::chrono::duration duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_point);
-    printf("[Iters: %d/%d] [Time: %.4fs/%.2f] [Wall Time: %.4fs]\n", iters, params.iters, world->time, params.target_time, (double) duration.count() / 1000);
+    if (render_interval_ok) {
+      std::chrono::duration duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start_point);
+      printf("[Iters: %d/%d] [Time: %.4fs/%.2f] [Wall Time: %.4fs]\n", iters, params.iters, world->time, params.target_time, (double) duration.count() / 1000);
+    }
 
     if (render_interval_ok && params.data_file_out) {
       world->timer_start("Save Frame");
